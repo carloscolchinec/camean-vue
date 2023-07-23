f4d555afa452fe3634299becf0b2adb840a6d8b4
Carlos Colcha <carlosarielcb3@gmail.com>
2023-07-22 21:30:53 -0500

    first commit

--- app.js
+++ app.js
@@ -2,92 +2,64 @@ Vue.config.productionTip = false;
 
 new Vue({
   el: "#app",
-  data() {
-    return {
-      scannerActive: false,
-      detectedBarcode: null,
-      lastDetectedBarcode: null,
-    };
-  },
   mounted() {
     this.initScanner();
   },
   methods: {
     initScanner() {
-      Quagga.init(
-        {
-          inputStream: {
-            name: "Live",
-            type: "LiveStream",
-            target: document.querySelector(".camera-view"),
-            constraints: {
-              facingMode: "environment", // Use the rear camera (or change to "user" for the front camera)
-            },
-            area: { // Limit the scanning area to a portion of the camera view
-              top: "30%", // Adjust this value based on your needs
-              right: "0%",
-              left: "0%",
-              bottom: "30%",
-            },
-          },
-          decoder: {
-            readers: ["ean_reader"], // You can add more reader formats if needed
-            multiple: false, // Only detect one barcode at a time for better performance
-          },
-          locate: true, // Enable localization of the barcode in the image
-        },
-        (err) => {
-          if (err) {
-            console.error("Error al inicializar Quagga:", err);
-            return;
-          }
-          console.log("Quagga inicializado correctamente.");
-          Quagga.onProcessed(this.drawDetectedRectangle);
-          this.startScanner();
-        }
-      );
+      const cameraView = document.querySelector(".camera-view");
+      const cameraVideo = document.querySelector(".camera-video");
+      const cameraOverlay = document.querySelector(".camera-overlay");
+      const barcodeNumber = document.createElement("div");
 
-      Quagga.onDetected(this.onBarcodeDetected);
-    },
-    startScanner() {
-      this.scannerActive = true;
-      Quagga.start();
-    },
-    onBarcodeDetected(result) {
-      if (result && result.codeResult.code) {
-        const detectedCode = result.codeResult.code;
-        if (detectedCode !== this.lastDetectedBarcode) {
-          this.detectedBarcode = detectedCode;
-          this.lastDetectedBarcode = detectedCode;
-          console.log("Código de barras detectado:", detectedCode);
+      const canvasContext = cameraOverlay.getContext("2d");
+      let scannerActive = true;
+
+      cameraView.appendChild(barcodeNumber);
+      barcodeNumber.classList.add("barcode-number");
+
+      async function scanBarcode() {
+        if (!scannerActive) return;
+
+        try {
+          const codeReader = new ZXing.BrowserBarcodeReader();
+          const result = await codeReader.decodeFromStream(
+            { video: { facingMode: "environment" } },
+            cameraVideo
+          );
+
+          if (result && result.text) {
+            drawDetectedBarcode(result.text);
+          }
+
+          // Continuar escaneando
+          scanBarcode();
+        } catch (err) {
+          console.error("Error al escanear:", err);
         }
       }
-    },
-    drawDetectedRectangle(result) {
-      if (!this.scannerActive) return;
-      if (result) {
-        const drawingCtx = Quagga.canvas.ctx.overlay;
-        const drawingCanvas = Quagga.canvas.dom.overlay;
-        drawingCtx.clearRect(
-          0,
-          0,
-          parseInt(drawingCanvas.getAttribute("width")),
-          parseInt(drawingCanvas.getAttribute("height"))
+
+      function drawDetectedBarcode(detectedCode) {
+        canvasContext.clearRect(0, 0, cameraOverlay.width, cameraOverlay.height);
+
+        if (!detectedCode) return;
+
+        // Dibujar rectángulo
+        canvasContext.strokeStyle = "#f00";
+        canvasContext.lineWidth = 2;
+        canvasContext.strokeRect(
+          cameraOverlay.width / 4,
+          cameraOverlay.height / 4,
+          cameraOverlay.width / 2,
+          cameraOverlay.height / 2
         );
 
-        if (result.boxes) {
-          result.boxes.forEach((box) => {
-            drawingCtx.strokeStyle = "#f00";
-            drawingCtx.lineWidth = 2;
-            drawingCtx.strokeRect(
-              box.x,
-              box.y,
-              box.width,
-              box.height
-            );
-          });
-        }
+        // Dibujar número de código de barras arriba del rectángulo
+        barcodeNumber.textContent = "[" + detectedCode + "]";
       }
+
+      // Iniciar el escaneo automáticamente
+      scanBarcode();
     },
   },
 });
