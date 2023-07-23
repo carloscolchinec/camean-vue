58927ebfa34748e921923846b387019d7d13a24f
Carlos Colcha <carlosarielcb3@gmail.com>
2023-07-22 21:23:59 -0500

    first commit

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ new Vue({
     return {
       scannerActive: false,
       detectedBarcode: null,
+      lastDetectedBarcode: null,
     };
   },
   mounted() {
@@ -22,10 +23,18 @@ new Vue({
             constraints: {
               facingMode: "environment", // Use the rear camera (or change to "user" for the front camera)
             },
+            area: { // Limit the scanning area to a portion of the camera view
+              top: "30%", // Adjust this value based on your needs
+              right: "0%",
+              left: "0%",
+              bottom: "30%",
+            },
           },
           decoder: {
             readers: ["ean_reader"], // You can add more reader formats if needed
+            multiple: false, // Only detect one barcode at a time for better performance
           },
+          locate: true, // Enable localization of the barcode in the image
         },
         (err) => {
           if (err) {
@@ -45,8 +54,14 @@ new Vue({
       Quagga.start();
     },
     onBarcodeDetected(result) {
-      console.log("Código de barras detectado:", result.codeResult.code);
-      this.detectedBarcode = result.codeResult;
+      if (result && result.codeResult.code) {
+        const detectedCode = result.codeResult.code;
+        if (detectedCode !== this.lastDetectedBarcode) {
+          this.detectedBarcode = detectedCode;
+          this.lastDetectedBarcode = detectedCode;
+          console.log("Código de barras detectado:", detectedCode);
+        }
+      }
     },
     drawDetectedRectangle(result) {
       if (!this.scannerActive) return;
