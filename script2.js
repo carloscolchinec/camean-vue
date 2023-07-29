64cfd09ea998b8a98f8fabf4625ccebd78b722f8
carloscolchinec <75394232+carloscolchinec@users.noreply.github.com>
2023-07-29 15:15:25 -0500

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,167 +1,139 @@
-// ______   ______  __       __    __ ________ ________         ______   ______  ________ ________ __       __  ______  _______  ________
-// /      \ /      \|  \     |  \  |  |        |        \       /      \ /      \|        |        |  \  _  |  \/      \|       \|        \
-// |  $$$$$$|  $$$$$$| $$     | $$\ | $| $$$$$$$$\$$$$$$$$      |  $$$$$$|  $$$$$$| $$$$$$$$\$$$$$$$| $$ / \ | $|  $$$$$$| $$$$$$$| $$$$$$$$
-// | $$   \$| $$  | $| $$     | $$$\| $| $$__      | $$         | $$___\$| $$  | $| $$__      | $$  | $$/  $\| $| $$__| $| $$__| $| $$__
-// | $$     | $$  | $| $$     | $$$$\ $| $$  \     | $$          \$$    \| $$  | $| $$  \     | $$  | $$  $$$\ $| $$    $| $$    $| $$  \
-// | $$   __| $$  | $| $$     | $$\$$ $| $$$$$     | $$          _\$$$$$$| $$  | $| $$$$$     | $$  | $$ $$\$$\$| $$$$$$$| $$$$$$$| $$$$$
-// | $$__/  | $$__/ $| $$_____| $$ \$$$| $$_____   | $$         |  \__| $| $$__/ $| $$        | $$  | $$$$  \$$$| $$  | $| $$  | $| $$_____
-// \$$    $$\$$    $| $$     | $$  \$$| $$     \  | $$          \$$    $$\$$    $| $$        | $$  | $$$    \$$| $$  | $| $$  | $| $$     \
-//  \$$$$$$  \$$$$$$ \$$$$$$$$\$$   \$$\$$$$$$$$   \$$           \$$$$$$  \$$$$$$ \$$         \$$   \$$      \$$\$$   \$$\$$   \$$\$$$$$$$$
 
-//                                             Todos los derechos reservados © COLNET 2023
-
-window.addEventListener("load", function () {
+window.addEventListener('load', function () {
     let selectedDeviceId;
     const codeReader = new ZXing.BrowserMultiFormatReader();
-    const canvas = document.getElementById("scanner-overlay");
-    const ctx = canvas.getContext("2d");
-    const video = document.getElementById("video");
-    const barcodeBox = document.querySelector(".barcode-box");
-    const badgeContainer = document.getElementById("badgeContainer");
-    const scanSound = document.getElementById("scanSound");
+    const canvas = document.getElementById('scanner-overlay');
+    const ctx = canvas.getContext('2d');
+    const detectedBarcode = document.getElementById('detectedBarcode');
+    const video = document.getElementById('video');
+    const barcodeBox = document.querySelector('.barcode-box');
+    const badgeContainer = document.getElementById('badgeContainer');
+    const scanSound = document.getElementById('scanSound');
+    const maxVerificationCount = 3;
+    const barcodeVerificationCount = {};
+
 
     function adjustBarcodeBox(x, y, width, height) {
+        // Mostrar el rectángulo del tamaño y posición del código de barras detectado
         barcodeBox.style.top = `${y}px`;
         barcodeBox.style.left = `${x}px`;
         barcodeBox.style.width = `${width}px`;
         barcodeBox.style.height = `${height}px`;
-        barcodeBox.style.display = "block";
+        barcodeBox.style.display = 'block';
     }
 
     function resetCanvas() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        barcodeBox.style.display = "none";
+        detectedBarcode.style.display = 'none';
+        barcodeBox.style.display = 'none';
     }
 
     function adjustLinePosition() {
         const videoHeight = video.clientHeight;
-        const animatedLine = document.querySelector(".animated-line");
+        const animatedLine = document.querySelector('.animated-line');
         animatedLine.style.top = `${videoHeight / 2}px`;
         sourceSelectPanel.style.marginTop = `${videoHeight + 20}px`;
     }
 
     function preloadScanSound() {
         const scanSound = new Howl({
-            src: ["./barcode.mp3"],
+            src: ['./barcode.mp3'],
             volume: 0.5,
             autoplay: true,
             onload: function () {
                 // Esta función se ejecuta cuando el sonido ha sido cargado
-                console.log("Sonido cargado.");
+                console.log('Sonido cargado.');
             },
             onloaderror: function (id, error) {
                 // Esta función se ejecuta si ocurre un error al cargar el sonido
-                console.error("Error al cargar el sonido:", error);
-            },
+                console.error('Error al cargar el sonido:', error);
+            }
         });
     }
 
-    codeReader
-    .listVideoInputDevices()
-    .then((videoInputDevices) => {
-      const sourceSelect = document.getElementById("sourceSelect");
-      selectedDeviceId = videoInputDevices[0].deviceId;
-      if (videoInputDevices.length >= 1) {
-        // Show all cameras in the sourceSelect dropdown
-        videoInputDevices.forEach((element) => {
-          const sourceOption = document.createElement("option");
-          sourceOption.text = element.label;
-          sourceOption.value = element.deviceId;
-          sourceSelect.appendChild(sourceOption);
-        });
 
-        // If it's a mobile device, try to select the rear camera
-        function isMobileDevice() {
-          return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-            navigator.userAgent
-          );
-        }
+    codeReader.listVideoInputDevices()
+        .then((videoInputDevices) => {
+            const sourceSelect = document.getElementById('sourceSelect');
+            selectedDeviceId = videoInputDevices[0].deviceId;
+            if (videoInputDevices.length >= 1) {
 
-        if (isMobileDevice()) {
-          const rearCameraDevices = videoInputDevices.filter(
-            (device) =>
-              device.label.includes("back") || device.label.includes("trasera")
-          );
-          selectedDeviceId =
-            rearCameraDevices.length > 0
-              ? rearCameraDevices[0].deviceId
-              : videoInputDevices[0].deviceId;
-        }
+                
+                videoInputDevices.forEach((element) => {
+                    const sourceOption = document.createElement('option');
+                    sourceOption.text = element.label;
+                    sourceOption.value = element.deviceId;
+                    sourceSelect.appendChild(sourceOption);
+                });
 
-        sourceSelect.value = selectedDeviceId;
+                sourceSelect.onchange = () => {
+                    codeReader.reset();
+                    selectedDeviceId = sourceSelect.value;
+                    resetCanvas();
+                    console.log(`Restarted with camera id ${selectedDeviceId}`);
+                    codeReader.decodeFromVideoDevice(selectedDeviceId, 'video', onScanResult, err => {
+                        if (!(err instanceof ZXing.NotFoundException)) {
+                            console.error(err);
+                        }
+                    });
+                };
 
-        sourceSelect.onchange = () => {
-          codeReader.reset();
-          selectedDeviceId = sourceSelect.value;
-          resetCanvas();
-          console.log(`Restarted with camera id ${selectedDeviceId}`);
-          codeReader.decodeFromVideoDevice(
-            selectedDeviceId,
-            "video",
-            onScanResult,
-            (err) => {
-              if (!(err instanceof ZXing.NotFoundException)) {
-                console.error(err);
-              }
+                const sourceSelectPanel = document.getElementById('sourceSelectPanel');
+                sourceSelectPanel.style.display = 'block';
             }
-          );
-        };
 
-        const sourceSelectPanel = document.getElementById("sourceSelectPanel");
-        sourceSelectPanel.style.display = "block";
+            function onScanResult(result, err) {
+                if (result) {
+                    resetCanvas();
 
-        setTimeout(() => {
-          const video = document.getElementById("video");
-          video.style.border = "1px solid #fe8e14";
+                    preloadScanSound();
 
-          const animatedLine = document.querySelector(".animated-line");
-          animatedLine.style.backgroundColor = "#fe8e14";
-        }, 700);
-      }
+                    badgeContainer.textContent = `Codigo Escaneado: ${result.text}`;
 
-      function onScanResult(result, err) {
-        if (result) {
-          resetCanvas();
+                    // Mostrar el rectángulo alrededor del código de barras detectado
+                    if (result.barcodeResult) {
+                        const { x, y, width, height } = getBoundingBox(result.barcodeResult.resultPoints);
+                        ctx.strokeStyle = '#10B981';
+                        ctx.lineWidth = 2;
+                        ctx.strokeRect(x, y, width, height);
 
-          preloadScanSound();
+                        // Mostrar el código detectado en la esquina superior derecha del rectángulo
+                        detectedBarcode.textContent = `[${result.text}]`;
+                        detectedBarcode.style.top = `${y}px`;
+                        detectedBarcode.style.left = `${x + width}px`;
+                        detectedBarcode.style.display = 'block';
 
-          badgeContainer.textContent = `Codigo Escaneado: ${result.text}`;
+                        // Mostrar el rectángulo que encierra el código de barras
+                        adjustBarcodeBox(x, y, width, height);
 
-          if (result.barcodeResult) {
-            if (!scannedNumbers[result.text]) {
-              scannedNumbers[result.text] = [];
+                        // Guardar el número escaneado en el array
+                        if (!scannedNumbers[result.text]) {
+                            scannedNumbers[result.text] = [];
+                        }
+                        scannedNumbers[result.text].push(new Date());
+
+                    }
+                }
+                if (err && !(err instanceof ZXing.NotFoundException)) {
+                    console.error(err);
+                    document.getElementById('badgeContainer').textContent = err;
+                }
             }
-            scannedNumbers[result.text].push(new Date());
-          }
-        }
-        if (err && !(err instanceof ZXing.NotFoundException)) {
-          console.error(err);
-          document.getElementById("badgeContainer").textContent = err;
-        }
-      }
 
-      video.onloadedmetadata = adjustLinePosition;
-      window.addEventListener("resize", adjustLinePosition);
+            video.onloadedmetadata = adjustLinePosition;
+            window.addEventListener('resize', adjustLinePosition);
 
-      codeReader.decodeFromVideoDevice(
-        selectedDeviceId,
-        "video",
-        onScanResult,
-        (err) => {
-          if (!(err instanceof ZXing.NotFoundException)) {
+            codeReader.decodeFromVideoDevice(selectedDeviceId, 'video', onScanResult, err => {
+                if (!(err instanceof ZXing.NotFoundException)) {
+                    console.error(err);
+                    document.getElementById('badgeContainer').textContent = err;
+                }
+            });
+            console.log(`Started continuous decode from camera with id ${selectedDeviceId}`);
+        })
+        .catch((err) => {
             console.error(err);
-            document.getElementById("badgeContainer").textContent = err;
-          }
-        }
-      );
-      console.log(
-        `Started continuous decode from camera with id ${selectedDeviceId}`
-      );
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+        });
 });
 
 // Función para obtener el rectángulo del código de barras a partir de los puntos de resultado
@@ -171,7 +143,7 @@ function getBoundingBox(resultPoints) {
     let maxX = Number.MIN_VALUE;
     let maxY = Number.MIN_VALUE;
 
-    resultPoints.forEach((point) => {
+    resultPoints.forEach(point => {
         minX = Math.min(minX, point.x);
         minY = Math.min(minY, point.y);
         maxX = Math.max(maxX, point.x);
