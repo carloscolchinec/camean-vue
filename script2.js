c28ccd14c12b307b2348755c841b45efdb2d2911
carloscolchinec <75394232+carloscolchinec@users.noreply.github.com>
2023-07-29 11:33:07 -0500

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,4 +1,4 @@
-                                                                                                                                window.addEventListener('load', function () {
+window.addEventListener('load', function () {
     let selectedDeviceId;
     const codeReader = new ZXing.BrowserMultiFormatReader();
     const canvas = document.getElementById('scanner-overlay');
